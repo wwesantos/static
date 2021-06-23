c069964051e6827f32ca69ea6b064b52adb29628
Waldeilson Santos <wwesantos@gmail.com>
2021-06-23 13:29:31 -0300

    Million calculator

---
+++ infodados/blog/milhao/script.js
@@ -0,0 +1,161 @@
+const get = function (query) {
+  return document.querySelector(query);
+};
+
+// set the dimensions and margins of the graph
+const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+const initialValue = get('#valor-inicial');
+const monthlyValue = get('#valor-mensal');
+const milhao = get('#milhao');
+const interestRange = get('#taxa-juros');
+const interestValue = get('#juros-span');
+const evolution = get('#evolution');
+const canva = get('#dataviz');
+
+const MILLION = 1_000_000;
+
+const dateOptions = { year: 'numeric', month: 'long' };
+const currencyOptions = { style: 'currency', currency: 'BRL' };
+
+const addRows = function (data) {
+  data.forEach(function (element) {
+    var tr = document.createElement('tr');
+    tr.innerHTML =
+      '<td>' +
+      element.fortatedDate +
+      '</td><td>' +
+      element.formatedValue +
+      '</td>';
+    evolution.appendChild(tr);
+  });
+};
+
+const plotBars = function (data) {
+  // append the svg object to the body of the page
+  var svg = d3
+    .select('#dataviz')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  // X axis
+  var x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(
+      data.map(function (d) {
+        return d.fortatedDate;
+      })
+    )
+    .padding(0.2);
+  svg
+    .append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(x))
+    .selectAll('text')
+    .attr('transform', 'translate(-10,0)rotate(-45)')
+    .style('text-anchor', 'end');
+
+  // Add Y axis
+  var y = d3.scaleLinear().domain([0, MILLION]).range([height, 0]);
+  svg.append('g').call(d3.axisLeft(y));
+
+  // Bars
+  svg
+    .selectAll('mybar')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', function (d) {
+      return x(d.fortatedDate);
+    })
+    .attr('y', function (d) {
+      return y(d.value);
+    })
+    .attr('width', x.bandwidth())
+    .attr('height', function (d) {
+      return height - y(d.value);
+    })
+    .attr('fill', '#69b3a2');
+};
+
+get('#form-inputs').addEventListener('focusin', function (e) {
+  if (e.target.value == 0) {
+    if (e.target.id == 'valor-inicial') {
+      initialValue.value = '';
+    }
+    if (e.target.id == 'valor-mensal') {
+      monthlyValue.value = '';
+    }
+  }
+});
+
+get('#form-inputs').addEventListener('input', function (e) {
+  if (e.target.id == 'taxa-juros') {
+    interestValue.innerHTML = e.target.value + '%';
+  }
+
+  const anualInterestRate = Number(interestRange.value);
+  const initial = Number(initialValue.value.replace('.', ''));
+  const monthly = Number(monthlyValue.value.replace('.', ''));
+
+  milhao.value = '';
+  evolution.innerHTML = '';
+  canva.innerHTML = '';
+
+  if (monthly > 9 || initial > 99) {
+    const monthlyIncrement = (Number(anualInterestRate) / 100 + 1) ** (1 / 12);
+
+    let count = 0;
+    let result = initial;
+
+    while (result < MILLION) {
+      count++;
+      result = result * monthlyIncrement + monthly;
+    }
+
+    if (count > 12) {
+      let step = Math.trunc(count / 10);
+      let currentValue = initial;
+      let i = 0;
+      const data = [];
+      while (i <= count) {
+        if ((i % step == 0 && step < count - i) || count == i) {
+          const today = new Date();
+          const next = new Date(today.setMonth(today.getMonth() + i));
+          const nexDate = new Intl.DateTimeFormat('pt-BR', dateOptions).format(
+            next
+          );
+
+          const nexValue = new Intl.NumberFormat(
+            'pt-BR',
+            currencyOptions
+          ).format(currentValue);
+
+          data.push({
+            fortatedDate: nexDate,
+            formatedValue: nexValue,
+            value: currentValue,
+          });
+        }
+
+        currentValue = currentValue * monthlyIncrement + monthly;
+        i++;
+      }
+      plotBars(data);
+      addRows(data);
+    }
+
+    const today = new Date();
+
+    const next = new Date(today.setMonth(today.getMonth() + count));
+    milhao.value =
+      'Você atingirá R$ 1 milhão em ' +
+      new Intl.DateTimeFormat('pt-BR', dateOptions).format(next);
+  }
+});
